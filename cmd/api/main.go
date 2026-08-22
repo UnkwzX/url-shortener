9ed6473ba234f6b6bee112983513b14c76c6e0cf
unkwzx <ivalvolok@gmail.com>
2026-08-23 01:12:25 +0300

    Добавлен pgxpool, реализован контекст с таймаутом, реализован пул соединений, проверка подключения к БД

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+	"time"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/unkwzx/url-shortener/internal/config"
 )
 
@@ -20,5 +23,24 @@ func main() {
 		os.Exit(1)
 	}
 	slog.Info("Конфиг успешно загружен", "Port", cfg.ServerPort)
+
+	//Контекст с таймаутом 5с
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Пул соединений с Postgres
+	dbPool, err := pgxpool.New(ctx, cfg.DBURL)
+	if err != nil {
+		slog.Error("Ошибка создания пула", "error", err)
+		os.Exit(1)
+	}
+	defer dbPool.Close()
+
+	// Проверка подключения к БД
+	if err := dbPool.Ping(ctx); err != nil {
+		slog.Error("Ошибка подключения к БД", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Успешное подключение к БД")
 
 }
