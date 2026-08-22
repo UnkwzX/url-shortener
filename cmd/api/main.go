ae80efb8d079255667dc4b2029fd586a5f4128e3
unkwzx <ivalvolok@gmail.com>
2026-08-23 00:56:06 +0300

    Добавлено чтение конфига, обработка ошибки при чтении конфига, и логгирование

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	"github.com/unkwzx/url-shortener/internal/config"
 )
 
 func main() {
@@ -10,5 +12,13 @@ func main() {
 	//Создаю логгер и ставлю его по умолчанию
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	//читаем конфиг, обрабатываем ошибку и пишем лог
+	cfg, err := config.Load()
+	if err != nil {
+		slog.Error("Ошибка загрузки конфига!", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Конфиг успешно загружен", "Port", cfg.ServerPort)
 
 }
