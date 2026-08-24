3ede2e2f689c5486c4ddcd58fb71ca6d02292662
unkwzx <ivalvolok@gmail.com>
2026-08-24 14:34:19 +0300

    Написаны sendJSON и sendError, пишущие в w

--- internal/handler/response.go
+++ internal/handler/response.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func sendJSON(w http.ResponseWriter, status int, data any) {
+	//через .header задаем заголовок, того что шлем json
+	w.Header().Set("Content-Type", "application/json")
+	//через .WriteHeader отправляем статус код 200, 400...
+	w.WriteHeader(status)
+
+	//сереализация data
+	json.NewEncoder(w).Encode(data)
+}
+
+func sendError(w http.ResponseWriter, status int, message string) {
+	mError := map[string]string{"error": message}
+
+	sendJSON(w, status, mError)
+}
