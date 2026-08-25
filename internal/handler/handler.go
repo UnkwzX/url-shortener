2eb83087c18b3dae0f85806f049a7b33c3dae0cc
unkwzx <ivalvolok@gmail.com>
2026-08-26 01:53:34 +0300

    создан CreateLink

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,62 @@
 package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/unkwzx/url-shortener/internal/service"
+)
+
+// от клиента
+type CreateLinkRequest struct {
+	URL      string `json:"url"`
+	ttlHours int    `json:"ttl_hours"`
+}
+
+// к клиенту
+type CreateLinkResponse struct {
+	Code     string `json:"code"`
+	ShortURL string `json:"short_url"`
+}
+
+type Handler struct {
+	service service.LinkService
+}
+
+func NewHandler(service service.LinkService) *Handler {
+	return &Handler{service: service}
+}
+
+// запрос -> h.service.CreateLink (генерация кода, сборка link) -> s.repo.Create(запись link в БД) -> ответ response
+func (h *Handler) CreateLink(w http.ResponseWriter, r *http.Request) {
+	var req CreateLinkRequest                   // сюда десериализуем тело запроса
+	err := json.NewDecoder(r.Body).Decode(&req) // десериализуем json
+	if err != nil {
+		sendError(w, http.StatusBadRequest, "invalid body request") // откдидываем ошибку
+		return                                                      // Выходим
+	}
+	// вызываем метод service CreateLink, передаем контекст, ссылку, срок жизни
+	link, err := h.service.CreateLink(r.Context(), req.URL, req.ttlHours)
+	//обрабатываем ошибку
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidURL) {
+			sendError(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		sendError(w, http.StatusInternalServerError, "error create url")
+		return
+	}
+	// формируем ответ
+	response := CreateLinkResponse{
+		Code:     link.Code,
+		ShortURL: fmt.Sprintf("https://localhost:8080/%s", link.Code),
+	}
+	// отправляем ответ клиенту
+	sendJSON(w, http.StatusCreated, response)
+}
+
+//Хендлер — это то, что обрабатывает входящий HTTP-запрос и пишет ответ.
+//w http.ResponseWriter — сюда пишется ответ (заголовки, статус, тело).
+//r *http.Request — сюда приходит вся информация о запросе (метод, URL, заголовки, тело, контекст).
